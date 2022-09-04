3a174eb9ebc413511e42cf892451e3467c73b212
tgunter <thothgunter@live.com>
2022-09-04 15:21:22 -0500

    Updated markdown parser and terminal coloring

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-
-
 //NOTE
 //all tab related logic and styling were originally made by 
 //https://www.w3schools.com/howto/howto_js_tabs.asp
@@ -9,7 +7,7 @@
 //
 
 //TODO
-//- tags are not good. they hsould be handelled the same way leadership principles are.
+//- tags are not good. they should be handelled the same way leadership principles are.
 //- handle toml parsing errors - this requires some altering of the toml crate
 //
 
@@ -19,6 +17,8 @@ use std::io::prelude::*;
 use serde_derive::Deserialize;
 use toml;
 use clap::{Command, Arg};
+use colored::*;
+use pulldown_cmark::{html, Options, Parser};
 
 const UNWRAP_DATE_FAIL :&'static str = "In input file date was not properly formatted.";
 
@@ -88,7 +88,11 @@ fn add_to_table(input: &Event, rt: &mut String){
         "offhours" => {
             *rt += &format!("\t<tr bgcolor={}>\n", OFFHOURS_COLOR);
         },
+        "" => {
+            *rt += "\t<tr>\n";
+        }
         _=> {
+            println!("{}: Tag is unknown {}.", "Error".red(), tag.yellow());
             *rt += "\t<tr>\n";
         }
     }
@@ -98,11 +102,14 @@ fn add_to_table(input: &Event, rt: &mut String){
     *rt += &format!("\t\t<td>{}/{}/{}</td>\n", date.month, date.day, date.year);
 
     *rt += &format!("\t\t<td>{}</td>\n", input.summary);
+
+
+    let details = markdown_to_html(input.details.as_ref().unwrap_or(&String::new()));
     *rt += &format!("\t\t<td> <div class=\"content hideContent\">{}</div>
 \t\t<div class=\"show-more\">
 \t\t    <a href=\"#\">Show more</a>
 \t\t</div>
-</td>\n", input.details.as_ref().unwrap_or(&String::new()));
+</td>\n", &details);
 
     let leadership_values = {
         let mut rt = String::new();
@@ -244,7 +251,7 @@ td {
 .hideContent {
     overflow: hidden;
     line-height: 1.3em;
-    height: 5.30em;
+    height: 5.20em;
 }
 
 .showContent {
@@ -497,8 +504,6 @@ fn main() {
             )
         .get_matches();
 
-
-
     
 
     let input_file_name =  matches.get_one::<String>("input");
@@ -539,10 +544,14 @@ fn main() {
 
 const TEST : &'static str = "
 [[events]]
-summary = \"On call work. Some strange ticket.\"
+summary = \"This is an example.\"
 date = 1979-05-27T07:32:00-08:00
 details = \"\"\" 
-Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut od
+This document was created using a proprietary tool found here, https://github.com/TKGgunter/AZTracker. <b>Testing</b>.
+
+### Header 3
+
+~Wild boy~
 \"\"\" 
 ownership = 1
 earn_trust = 1
@@ -582,13 +591,32 @@ are_right_alot = 1
 
 [[events]]
 summary = \"Some more news.\"
-date = 1979-06-27T07:32:00-08:00
+date = 2022-09-04
 details = \"\"\" 
 Totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut od
 \"\"\" 
 customer_obsession = 1
 bias_for_action = 1
 are_right_alot = 1
-
 ";
 
+
+fn markdown_to_html(input: &str)->String {
+    //NOTE 
+    //This is an example from
+    //https://github.com/raphlinus/pulldown-cmark/blob/master/examples/string-to-string.rs
+
+   
+    
+    // Set up options and parser. Strikethroughs are not part of the CommonMark standard
+    // and we therefore must enable it explicitly.
+    let mut options = Options::empty();
+    options.insert(Options::ENABLE_STRIKETHROUGH);
+    let parser = Parser::new_ext(input, options);
+
+
+    let mut html_output: String = String::with_capacity(input.len() * 3 / 2);
+    html::push_html(&mut html_output, parser);
+
+    return html_output;
+}
