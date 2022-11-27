2d116c5d900736c41e39a0cb363b9ac71c101d4b
Thoth Gunter <mtgunter@amazon.com>
2022-11-27 15:35:05 -0600

    added test

---
+++ src/test.rs
@@ -0,0 +1,59 @@
+
+pub const TEST : &'static str = "
+[[events]]
+summary = \"This is an example.\"
+date = 1979-05-27T07:32:00-08:00
+details = \"\"\" 
+This document was created using a proprietary tool found here, https://github.com/TKGgunter/AZTracker. <b>Testing</b>.
+
+### Header 3
+
+*Wild boy*
+\"\"\" 
+ownership = 1
+earn_trust = 1
+dive_deep = 1
+
+[[events]]
+summary = \"Other work.\"
+date = 1979-06-27T07:32:00-08:00
+details = \"\"\" 
+Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut od
+\"\"\" 
+deliver_results = 1
+bias_for_action = 1
+are_right_alot = 1
+tags = \"Bar raising\"
+
+[[events]]
+summary = \"Some next work.\"
+date = 1979-06-27T07:32:00-08:00
+details = \"\"\" 
+Totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut od
+\"\"\" 
+deliver_results = 1
+bias_for_action = 1
+are_right_alot = 1
+tags = \"invest\"
+
+[[events]]
+summary = \"Some more news.\"
+date = 1979-06-27T07:32:00-08:00
+details = \"\"\" 
+Totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut od. Totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut od
+\"\"\" 
+deliver_results = 1
+bias_for_action = 1
+are_right_alot = 1
+
+[[events]]
+summary = \"Some more news.\"
+date = 2022-09-04
+details = \"\"\" 
+Totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut od
+\"\"\" 
+customer_obsession = 1
+bias_for_action = 1
+are_right_alot = 1
+";
+
